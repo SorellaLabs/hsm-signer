25007afd22ce59864d901084b636cb61c5e976b1
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-06-10 17:19:18 -0400

    signing

--- src/lib.rs
+++ src/lib.rs
@@ -98,7 +98,7 @@ impl Pkcs11Signer {
     pub fn sign_message<B: AsRef<[u8]>>(&self, msg: B) -> Result<Vec<u8>, cryptoki::error::Error> {
         let lock = self.session.lock();
         let out = lock
-            .sign(&Mechanism::EcdsaSha256, self.pk_handle, msg.as_ref())
+            .sign(&Mechanism::Ecdsa, self.pk_handle, msg.as_ref())
             .unwrap();
         drop(lock);
         Ok(out)
